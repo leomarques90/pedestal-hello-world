cd978a3ff8d0c1b2cfd6bc0dfaffbae23f4e7fe3
leonardo.silva7 <leonardo.silva@brasilparalelo.com.br>
2021-12-22 15:50:40 -0300

    query-params, response functions, domain logic and conditional response (cool)

--- src/hello.clj
+++ src/hello.clj
@@ -2,9 +2,26 @@
   (:require [io.pedestal.http :as http]
             [io.pedestal.http.route :as route]))
 
+(defn ok [body]
+  {:status 200 :body body})
+
+(defn not-found []
+  {:status 400 :body "Not found\n"})
+
+(def unmentionables #{"YHWH" "Voldemort" "Mxyzptlk" "Rumplestiltskin" "曹操"})
+
+(defn greeting-for [nm]
+  (cond
+    (unmentionables nm) nil
+    (empty? nm)         "Hello, world!\n"
+    :else               (str "Hello, " nm "\n")))
+
 (defn respond-hello [request]
-  {:status 200
-   :body "Alô, galera de caubói!"})
+  (let [nm (get-in request [:query-params :name])
+        resp (greeting-for nm)]
+    (if resp
+      (ok resp)
+      (not-found))))
 
 (def routes
   (route/expand-routes
@@ -16,5 +33,25 @@
     ::http/type   :jetty
     ::http/port   8890}))
 
+(def service-map
+  {::http/routes routes
+   ::http/type   :jetty
+   ::http/port   8890})
+
 (defn start []
-  (http/start (create-server)))
+  (http/start (http/create-server service-map)))
+
+(defonce server (atom nil))
+
+(defn start-dev []
+  (reset! server
+          (http/start (http/create-server
+                       (assoc service-map
+                              ::http/join? false)))))
+
+(defn stop-dev []
+  (http/stop @server))
+
+(defn restart []
+  (stop-dev)
+  (start-dev))
