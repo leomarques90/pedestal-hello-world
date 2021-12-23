296a1c576c144348ec0031b367c3bd56523b6e0d
leonardo.silva7 <leonardo.silva@brasilparalelo.com.br>
2021-12-23 12:16:53 -0300

    working with interceptors, transform requests, adding responses

--- src/hello.clj
+++ src/hello.clj
@@ -1,14 +1,18 @@
 (ns hello
-  (:require [io.pedestal.http :as http]
-            [io.pedestal.http.route :as route]))
-
-(defn ok [body]
-  {:status 200 :body body})
-
-(defn not-found []
-  {:status 400 :body "Not found\n"})
+  (:require [clojure.data.json :as json]
+            [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]
+            [io.pedestal.http.content-negotiation :as conneg]))
 
 (def unmentionables #{"YHWH" "Voldemort" "Mxyzptlk" "Rumplestiltskin" "曹操"})
+
+(defn ok [body]
+  {:status 200
+   :body   body})
+
+(defn not-found []
+  {:status 400
+   :body   "Not found\n"})
 
 (defn greeting-for [nm]
   (cond
@@ -17,15 +21,52 @@
     :else               (str "Hello, " nm "\n")))
 
 (defn respond-hello [request]
-  (let [nm (get-in request [:query-params :name])
+  (let [nm   (get-in request [:query-params :name])
         resp (greeting-for nm)]
     (if resp
       (ok resp)
       (not-found))))
 
+(def echo
+  {:name ::echo
+   :enter #(assoc % :response (ok (:request %)))})
+
+(def supported-types ["text/html" "application/edn" "application/json" "text/plain"])
+
+(def content-neg-intc (conneg/negotiate-content supported-types))
+
+(defn accepted-type
+  [context]
+  (get-in context [:request :accept :field] "text/plain"))
+
+(defn transform-content
+  [body content-type]
+  (case content-type
+    "text/html" body
+    "text/plain" body
+    "application/edn" (pr-str body)
+    "application/json" (json/write-str body)))
+
+(defn coerce-to
+  [response content-type]
+  (-> response
+      (update :body transform-content content-type)
+      (assoc-in [:headers "Content-Type"] content-type)))
+
+(def coerce-body
+  {:name ::coerce-body
+   :leave
+   (fn [context]
+     (cond-> context
+       (nil? (get-in context [:response :headers "Content-Type"]))
+       (update-in [:response] coerce-to (accepted-type context))))})
+
 (def routes
   (route/expand-routes
-   #{["/greet" :get respond-hello :route-name :greet]}))
+   #{["/greet" :get [coerce-body content-neg-intc respond-hello] :route-name :greet]
+     ["/echo"  :get echo]}))
+
+(def server (atom nil))
 
 (defn create-server []
   (http/create-server
@@ -33,25 +74,10 @@
     ::http/type   :jetty
     ::http/port   8890}))
 
-(def service-map
-  {::http/routes routes
-   ::http/type   :jetty
-   ::http/port   8890})
-
 (defn start []
-  (http/start (http/create-server service-map)))
+  (swap! server
+         (constantly (http/start (create-server))))
+  nil)
 
-(defonce server (atom nil))
-
-(defn start-dev []
-  (reset! server
-          (http/start (http/create-server
-                       (assoc service-map
-                              ::http/join? false)))))
-
-(defn stop-dev []
-  (http/stop @server))
-
-(defn restart []
-  (stop-dev)
-  (start-dev))
+(defn stop []
+  (swap! server http/stop))
